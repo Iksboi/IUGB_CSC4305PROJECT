47967895bef31cad8a77d45ac6754561f5e65582
Iksboi <130296326+Iksboi@users.noreply.github.com>
2023-11-03 19:16:09 +0000

    Add files via upload

---
+++ IUGB_CSC4305PROJECT/login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log in-IUGB</title>
+    <link rel="stylesheet" href=https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css>
+    <link rel="icon" href="C:\xampp\htdocs\school management system\img\Logo">
+</head>
+<body class="body-login">
+  
+    <div class="black-fill">
+        <br><br>
+        <div class="d-flex justify-content-center align-items-center flex-column">
+        <form class="login">
+            <div class="text-center"> 
+                <img src ="logo.png"
+                            width="100">
+            </div>
+            <h3>LOGIN</h3>
+            <div class="mb-3">
+                <label  class="form-label">Username</label>
+                <input type="text" class="form-control">
+
+            </div>
+            <div class="mb-3">
+                <label  class="form-label">Password</label>
+                <input type="password" class="form-control"> </div>
+                <div class="mb-3">
+                <label  class="form-label">Login As</label>
+                <select class="form-control">
+                    <option value="1">Admin</option> 
+                    <option value="1">Student</option> 
+                    <option value="1">Teacher</option> </select>
+               
+            <button type="submit" class="btn btn-primary">Login</button>
+            <a href="index.php" class="text-decoration-none"></a>
+            </form>
+                    <br>
+                    <div class="text-center text-light">
+                        <> Copyright &copy; 2023 International University of Grand Bassam. All rights reserved.  </>
+                    </div>
+                
+                </div>
+                </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js></script>
+</body>
+</html>
