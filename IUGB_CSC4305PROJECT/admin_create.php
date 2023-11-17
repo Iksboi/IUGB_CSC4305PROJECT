d9895e28e5bf659f97adc00365c0265196c30f0f
Iksboi <130296326+Iksboi@users.noreply.github.com>
2023-11-17 19:14:58 +0000

    ok

--- IUGB_CSC4305PROJECT/admin_create.php
+++ IUGB_CSC4305PROJECT/admin_create.php
@@ -9,18 +9,24 @@ if(isset($_POST['submit'])) {
     $firstname=$_POST['fname'];
     $lastname=$_POST['lname'];
     $date_of_birth=$_POST['date_of_birth'];
-    $id = "10" . substr(uniqid(), 7, 6);
+    $id = "10" . str_pad(mt_rand(0, 999999), 6, '0', STR_PAD_LEFT);
+
 
     
 
-    $sql="INSERT INTO `admin` (username, password, fname, lname, date_of_birth,admin_crea_date,u_id)
+    $sql="INSERT INTO `admin` (username, password, fname, lname, date_of_birth, admin_creat_date, u_id)
     
-    VALUES ('$username', '$password', '$firstname','$lastname', '$date_of_birth', NOW(),'id')";
+    VALUES ('$username', '$password', '$firstname','$lastname', '$date_of_birth', NOW(),'$id')";
+
     $result=mysqli_query($con,$sql);
 
     if($result){
+
        echo"data inserted successfully";
-    } else {
+    }
+     else 
+
+    {
         die(mysqli_error($con));
     }
 }
@@ -30,12 +36,13 @@ if(isset($_POST['submit'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add User - IUGB</title>
-    <link rel="stylesheet" href=https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css>
-    <link rel="icon" href="C:\xampp\htdocs\school management system\img\Logo">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Add User - IUGB</title>
+<link rel="stylesheet" href=https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css>
+<link rel="icon" href="C:\xampp\htdocs\school management system\img\Logo">
 </head>
+
 <body>
   
     <div class="container">
@@ -44,44 +51,58 @@ if(isset($_POST['submit'])) {
         ``
 
 
-              <form method="post" >
+            <form method="post">
             <div class="text-center"> 
-                <img src ="logo.png"
-                            width="100">
+            <img src ="logo.png"
+                 width="100">
+
             </div>
+
             <h3>Add a user</h3>
 
             <div class="mb-3">
+
                 <label  class="form-label">Username</label>
                 <input type="text" 
                 class="form-control"
                 name="username">
 
             </div>
+
             <div class="mb-3">
+
                 <label  class="form-label">Password</label>
                 <input type="password" 
                 class="form-control"
                 name="password"> 
+
             </div>
+
             <div class="mb-3">
+
                 <label  class="form-label">First name</label>
                 <input type="text" 
                 class="form-control"
                 name="fname"> 
+
             </div>
             <div class="mb-3">
+
                 <label  class="form-label">Last name</label>
                 <input type="text" 
                 class="form-control"
                 name="lname"> 
+
             </div>
             <div class="mb-3">
+
                 <label  class="form-label">Date of birth</label>
                 <input type="date" 
                 class="form-control"
                 name="date_of_birth"> 
+
             </div>
+
           
                
             <button type="submit" class="btn btn-primary" name="submit">Confirm</button>
@@ -89,8 +110,9 @@ if(isset($_POST['submit'])) {
             </form>
                     <br>
                     <div class="text-center text-light">
+
                         
-                        <> Copyright &copy; 2023 International University of Grand Bassam. All rights reserved.  </>
+                       <> Copyright &copy; 2023 International University of Grand Bassam. All rights reserved.  </>
                     </div>
                 
                 </div>
