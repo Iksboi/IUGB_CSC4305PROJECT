3e1a734259ba586b453ec62c48a13b6d20cfc4e2
Iksboi <130296326+Iksboi@users.noreply.github.com>
2023-11-24 17:15:14 +0000

    Logo fixed

--- req/admin/display.php
+++ req/admin/display.php
@@ -9,7 +9,7 @@ include 'C:\xampp\htdocs\IUGB_CSC4305PROJECT\DB_connection.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Operations</title>
     <link rel="stylesheet" href=https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css>
-    <link rel="icon" href="C:\xampp\htdocs\school management system\img\Logo">
+    <link rel="icon" href="img\logo\logo.png">
 </head>
 <body>
     
